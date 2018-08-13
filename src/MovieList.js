1818a01207a284c8755092ea9f2d9ac00a3fddf9
Chris <chrispackett1998@gmail.com>
2018-08-13 17:03:28 -0400

    implemented sorting by release date

--- src/MovieList.js
+++ src/MovieList.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react'
 import Movie from './Movie'
 import movies from './1989.json'
 
+    // let new_release_date = movies.results[0].release_date.split('-').join('')
+    // new_release_date = parseInt(new_release_date)
+    // console.log(new_release_date)
+    // sortedMovies = movies.results.map((movie) => {
+    //     movieDateNumber = parseInt(movie.release_date.split('-').join(''))
+    //     movieDateNumber.sort(sortByNumber)
+    // })
+
 class MovieList extends Component {
     render() {
-        // let new_release_date = movies.results.release_date.split('-').join()
-        // new_release_date = parseInt(new_release_date)
-        // console.log(new_release_date)
         return (
             <section>
                 <ul className="movie-list">
-                    {movies.results.map((movie, index) => {
+                    {movies.results.sort((a, b) => {
+                        if (a.release_date > b.release_date){
+                            return 1
+                        } else if (a.release_date < b.release_date){
+                            return -1
+                        } else {
+                            return 0
+                        }
+                    }).map((movie, index) => {
                         return <Movie 
                         title={movie.title} 
                         image={movie.poster_path} 
